b574df666894fbdbc4299dc5ac6963a1831c90bd
Marine Ropers <marine.ropers@laposte.net>
2020-12-03 19:21:11 +0100

    V3 Ajout fonctionnalités page produit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
-
+const euro = new Intl.NumberFormat('fr-FR', {
+  style: 'currency',
+  currency: 'EUR',
+  minimumFractionDigits: 2
+});
 
 let request = new XMLHttpRequest();
 request.open('GET', 'http://localhost:3000/api/furniture');
@@ -22,10 +26,13 @@ function displayMeuble(furniture) {
     return `<div class="col-12 col-md-6 col-lg-4">
                 <div class="card my-4 shadow-sm">
                     <img src="${furniture.imageUrl}" height="300">
-                    <a class="stretched-link text-dark" href="product.html?${furniture._id}"><h2 class="text-center my-3 meuble__name">${furniture.name}</h2></a>
-                    <p class="meuble__description">${furniture.description}</p>
-                    <p class="text-right meuble__price">${furniture.price}€</p>
+                    <div class="card-body">
+                        <a class="stretched-link text-dark" href="product.html?${furniture._id}">
+                            <h2 class="card-title text-center meuble__name">${furniture.name}</h2>
+                        </a>
+                        <p class="card-text px-3 text-justify meuble__description">${furniture.description}</p>
+                        <p class="card-text text-right font-weight-bold pr-3 meuble__price">${euro.format(furniture.price / 100)}</p>
+                    </div>
                 </div>
             </div>`
 };
-
--- js/product.js
+++ js/product.js
@@ -1,12 +1,15 @@
+const euro = new Intl.NumberFormat('fr-FR', {
+  style: 'currency',
+  currency: 'EUR',
+  minimumFractionDigits: 2
+});
+
 let requestProduct = new XMLHttpRequest();
 requestProduct.open('GET', 'http://localhost:3000/api/furniture/5be9cc611c9d440000c1421e');
 requestProduct.addEventListener('load', function() {
     if(requestProduct.status >= 200) {
         let furnitures = JSON.parse(requestProduct.responseText);
         let htmlVarnish = '';
-        console.log(requestProduct.responseText);
-
-        
 
         document.getElementById('furnitureImage').innerHTML = displayImage(furnitures);
 
@@ -19,26 +22,81 @@ requestProduct.addEventListener('load', function() {
         });
 
         document.getElementById('furnitureVarnish').innerHTML = htmlVarnish;
+
+        document.title = title(furnitures);
     }
 
 });
 requestProduct.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
 requestProduct.send();
 
+
+
+let request = new XMLHttpRequest();
+request.open('GET', 'http://localhost:3000/api/furniture');
+request.addEventListener('load', function() {
+    if(request.status >= 200) {
+        let furnitures = JSON.parse(request.responseText);
+        let product = '';
+        
+        furnitures.forEach((furniture) => {
+            product += carousel(furniture)
+        });
+
+        document.getElementById('carouselControls').innerHTML = product +  `<a class="carousel-control-prev" href="#carouselControls" role="button" data-slide="prev">
+                                                                                <span class="carousel-control-prev-icon mr-5" aria-hidden="true"></span>
+                                                                                <span class="sr-only">Précédent</span>
+                                                                            </a>
+                                                                            <a class="carousel-control-next" href="#carouselControls" role="button" data-slide="next">
+                                                                                <span class="carousel-control-next-icon ml-5" aria-hidden="true"></span>
+                                                                                <span class="sr-only">Suivant</span>
+                                                                            </a>`;
+    }
+});
+request.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+request.send();
+
+
 //  ---------------  FONCTIONS  ---------------  //
 
+function carousel(furniture) {
+   // if(furniture._id !== furnitures._id) {
+        return `<div class="col-md-2">
+                    <div class="card">
+                        <img src="${furniture.imageUrl}" class="d-block" height="100">
+                        <div class="card-body p-3 banner_card">                            
+                                <h5 class="card-title text-center banner_title mb-0">
+                                    <a class="stretched-link text-dark" href="product.html?${furniture._id}">
+                                        ${furniture.name}
+                                    </a>
+                                </h5>                            
+                            <p class="card-text text-center font-weight-bold">${euro.format(furniture.price / 100)}</p>
+                        </div>
+                    </div>
+                </div>`
+    //}
+}
+
 function displayDescription(furniture) {
-    return `<h2 class="text-center my-3 meuble__name">${furniture.name}</h2>
+    return `<h2 class="text-center text-dark font-weight-bold my-4 meuble__name">${furniture.name}</h2>
             <p class="meuble__description">${furniture.description}</p>`
 };
 
 function displayImage(furniture) {
-    return `<div class="card"><img src="${furniture.imageUrl}"></div>`
+    return `<div class="card"><img src="${furniture.imageUrl}">
+                <a href="${furniture.imageUrl}" data-toggle="lightbox">
+                    <i class="fas fa-search-plus mr-2 mb-2 text-light icon_zoom"></i>
+                </a>
+            </div>`
 };
 
 function displayPrice(furniture) {
-    return `<p class="text-right meuble__price">${furniture.price}€</p>`
+    return `<p class="text-right font-weight-bold mt-4 meuble__price">${euro.format(furniture.price / 100)}</p>`
 }
 function displayVarnish(varnish) {
    return `<option>${varnish}</option>`
+}
+
+function title(furniture) {
+    return `${furniture.name} - Orinoco`
 }
